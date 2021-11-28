f78e759de8718921cfb1449f1dd9c4c8e70eeb39
zhaoliang92 <89258158+zhaoliang92@users.noreply.github.com>
2021-11-27 20:09:54 -0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,49 +1,33 @@
 import os
 import telegram
 from telegram.ext import Updater, CommandHandler, ConversationHandler, CallbackQueryHandler, MessageHandler, Filters
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton, chataction, InputMediaPhoto
-from requests import get
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton, chataction
 
 INPUT_TEXT = 0
 
-randomPeopleUrl = "https://images2.imgbox.com/ae/80/bRrO7kaP_o.jpeg"
-
-image = get(randomPeopleUrl).content
-
-
-# ENLACE DE CANALES O GRUPOS DE LA BOTONERA
 
 def start(update, context):
 
     button1 = InlineKeyboardButton(
-        text="Dojo en TV",
-        url="https://t.me/dojo_en_tv"
+        text="Nombre canal o grupo",
+        url="https://www.canal-o-grupo-de-telegram.com"
     )
 
     button2 = InlineKeyboardButton(
-        text="Instituto Confucio, UH",
-        url="https://t.me/ConfucioUH"
-    )
-
-    button4 = InlineKeyboardButton(
-        text="🏮哈瓦那的唐人街🏮Barrio Chino de Zanja",
-        url="https://t.me/Barrio_Chino_Cuba"
+        text="Nombre canal o grupo",
+        url="https://www.canal-o-grupo-de-telegram.com"
     )
 
     button3 = InlineKeyboardButton(
         text="Unete a la botonera",
-        url="https://t.me/Adrian_Cabrales_Balsa"
+        url="https://t.me/cheng_zhi"
     )
-    
-    context.bot.sendMediaGroup(chat_id=update.effective_chat.id, media=[InputMediaPhoto
-        (image, caption="")])
 
-# NOMBRE Y BOTONES DE LA BOTONERA
     update.message.reply_text(
-        text='"INFINITE KEYPAD"',
+        text='"PHOTO Y NOMBRE DE LA BOTONERA SI DESEA"',
         reply_markup=InlineKeyboardMarkup([
-            [button1, button2],
-            [button4],
+            [button1],
+            [button2],
             [button3]
             ])
     )
